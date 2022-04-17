0396ba0c9bdd5849e5551ea95e8cd1fda4998379
krypton225 <k.mohamed.11.98@gmail.com>
2022-04-17 16:38:33 +0200

    ADD @Assuit UNI sheet => (1) problem

---
+++ src/ICPC_Assuit_Community/Say_Hello.java
@@ -0,0 +1,17 @@
+/*
+* Hello! This is a solution of the problem which its links is:
+* https://codeforces.com/group/MWSDmqGsZm/contest/219158/submission/48945202
+* */
+
+package ICPC_Assuit_Community;
+
+import java.util.Scanner;
+
+public class Say_Hello {
+    private static final Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String name = s.next();
+        System.out.println("Hello, " + name);
+    }
+}

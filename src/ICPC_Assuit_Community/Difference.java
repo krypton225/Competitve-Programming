937957a2f0cffabfa516a8fafc4a4c734eb9c9cf
krypton225 <k.mohamed.11.98@gmail.com>
2022-04-19 16:19:43 +0200

    ADD @ Assuit UNI sheet => (4) problem

---
+++ src/ICPC_Assuit_Community/Difference.java
@@ -0,0 +1,23 @@
+/*
+ * Hello! This is a solution of the problem which its links is:
+ * https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+ * */
+
+package ICPC_Assuit_Community;
+
+import java.util.Scanner;
+
+public class Difference {
+
+    private static final Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        long a = s.nextLong();
+        long b = s.nextLong();
+        long c = s.nextLong();
+        long d = s.nextLong();
+
+        long diff = (a * b) - (c * d);
+        System.out.println("Difference = " + diff);
+    }
+}

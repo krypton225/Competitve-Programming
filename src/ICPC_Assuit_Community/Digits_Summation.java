7479454c07590e549231722e0c788f8f46bd1397
krypton225 <k.mohamed.11.98@gmail.com>
2022-04-19 16:24:12 +0200

    ADD @ Assuit UNI sheet => (6) problem

---
+++ src/ICPC_Assuit_Community/Digits_Summation.java
@@ -0,0 +1,21 @@
+/*
+ * Hello! This is a solution of the problem which its links is:
+ * https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+ * */
+
+package ICPC_Assuit_Community;
+
+import java.util.Scanner;
+
+public class Digits_Summation {
+
+    private static final Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        long x = s.nextLong();
+        long y = s.nextLong();
+
+        long sum = (x % 10) + (y % 10);
+        System.out.println(sum);
+    }
+}

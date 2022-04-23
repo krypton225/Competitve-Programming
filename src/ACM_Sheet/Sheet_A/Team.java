373df83e4adbf1d54f9a3db088ecd157d732f04b
krypton225 <k.mohamed.11.98@gmail.com>
2022-04-23 05:49:59 +0200

    AFTER COMMIT, ADD @ ACM_Sheet @ Sheet_A => (4) problem

--- src/ACM_Sheet/Sheet_A/Team.java
+++ src/ACM_Sheet/Sheet_A/Team.java
@@ -1,2 +1,30 @@
-package ACM_Sheet.Sheet_A;public class Team {
+/*
+ * Hello! This is a solution of the problem which its links is:
+ * https://codeforces.com/contest/231/problem/A
+ * */
+
+package ACM_Sheet.Sheet_A;
+
+import java.util.Scanner;
+
+public class Team {
+
+    private static final Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numberOfProblems = s.nextInt();
+        int solutionResult = 0;
+
+        for (int i = 0; i < numberOfProblems; i++) {
+            int petya = s.nextInt();
+            int vasya = s.nextInt();
+            int tonya = s.nextInt();
+
+            if((petya + vasya + tonya) >= 2) {
+                solutionResult++;
+            }
+        }
+
+        System.out.println(solutionResult);
+    }
 }

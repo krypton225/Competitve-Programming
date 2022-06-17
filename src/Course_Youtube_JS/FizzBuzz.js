bd46eb56d199a8e7e5226c6a7e2daf77ae301de1
krypton225 <k.mohamed.11.98@gmail.com>
2022-06-17 14:14:43 +0200

    EDIT @ FizzBuzz.js in start loop var

--- src/Course_Youtube_JS/FizzBuzz.js
+++ src/Course_Youtube_JS/FizzBuzz.js
@@ -1,5 +1,5 @@
 const fizzBuzzFunction = (myNum) => {
-  for (let i = 0; i <= myNum; i++) {
+  for (let i = 1; i <= myNum; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("fizzbuzz");
     } else if (i % 5 === 0) {
